910e45a95720e4bfcae6b51be850f6b226ab6283
proliferam <conta45455@gmail.com>
2026-01-15 20:43:33 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import { criarPagamentoPix } from "./mercadopago.js";
 import { criarPedido } from "./database.js";
 
+
 const app = express();
 app.use(cors());
 app.use(express.json());
